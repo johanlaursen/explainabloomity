eea2fa83c6923eaf48e9cdaf2e7a631fadb03522
johanlaursen <52320383+johanlaursen@users.noreply.github.com>
2024-02-05 12:27:45 +0100

    add utils

---
+++ utils.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def visualize_single(att_map, sentence, figname):
+    """
+    Attention map for a given layer and head
+    """
+    
+    plt.figure(figsize=(16, 12))
+    plt.imshow(att_map, cmap='Reds')
+    plt.xticks(range(len(sentence)), sentence, rotation=60, fontsize=12)
+    plt.yticks(range(len(sentence)), sentence, fontsize=12)
+
+    plt.grid(False)
+    plt.savefig(figname, dpi=400)
+
+    
