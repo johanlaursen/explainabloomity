a69e644072adc3de9c1b997eef4d1de46347466f
Johanlaursen <johan7laursen@gmail.com>
2024-03-21 12:55:34 +0100

    run_prune_jobs.sh

---
+++ run_prune_jobs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# model_name="bigscience/bloom-7b1"
+# model_name="facebook/opt-6.7b"
+model_name="facebook/opt-13b"
+
+model_basename="${model_name##*/}"
+path="/home/data_shares/mapillary/thesis_models/pruned_models"
+pathpruned="${path}/${model_basename}_pruned"
+
+metrics=(
+    "cosine"
+    "euclidean"
+    "random"
+)
+prune_percents=(
+    "0.25"
+    "0.5"
+    "0.75"
+)
+
+for metric in "${metrics[@]}"
+do
+    for prune_percent in "${prune_percents[@]}"
+    do
+        sbatch --job-name="prune_${model_basename}_${metric}_${prune_percent}" prune.job $prune_percent $metric $model_name $pathpruned
+    done
+done
