cefc4cb3c29eae97066386aad6778411f5240475
Ispanicus <36916264+Ispanicus@users.noreply.github.com>
2024-04-05 09:59:54 +0200

    added plotting functionality

--- run_prune_jobs.sh
+++ run_prune_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-model_name="bigscience/bloom-7b1"
+# model_name="bigscience/bloom-7b1"
 # model_name="facebook/opt-6.7b"
-# model_name="facebook/opt-13b"
+model_name="facebook/opt-13b"
 
 model_basename="${model_name##*/}"
 path="/home/data_shares/mapillary/thesis_models/pruned_models/"
@@ -16,34 +16,18 @@ prune_methods=(
 )
 metrics=(
     "cosine"
-    # "euclidean"
+    "euclidean"
 )
 prunetasks=(
-    "paws_en"
+    #"paws_en"
     # "hellaswag"
-    # "arc_easy"
+    "arc_easy"
 )
 prune_percents=(
     "0.25"
-    # "0.5"
-    # "0.75"
+    "0.5"
+    "0.75"
 )
-
-# for metric in "${metrics[@]}"
-# do
-#     for prune_percent in "${prune_percents[@]}"
-#     do
-#         case "$prunetype" in
-#             prune)
-#                 sbatch --job-name="${prunetype}_${model_basename}_${metric}_${prune_percent}" prune.job $prune_percent $metric $model_name $pathpruned
-#                 ;;
-#             unbalanced_prune)
-#                 sbatch --job-name="${prunetype}_${model_basename}_${metric}_${prune_percent}" unbalanced_prune.job $prune_percent $metric $model_name $pathpruned
-#                 ;;
-#         esac
-#         # Note if it is balanced or unbalanced
-#     done
-# done
 
 for metric in "${metrics[@]}"
 do
@@ -54,7 +38,7 @@ do
             for prunemethod in "${prune_methods[@]}"
             do
                 sbatch --job-name="${prunemethod}_${model_basename}_${metric}_${prune_percent}_${prompt}" prune.job $prune_percent $metric $model_name $path $prunetask $metric $prunemethod
-                # sbatch --job-name="${prunetype}_${model_basename}_${metric}_${prune_percent}_${prompt}" prune.job $prune_percent $metric $model_name $path $prunetask "random" $prunemethod
+                sbatch --job-name="${prunetype}_${model_basename}_${metric}_${prune_percent}_${prompt}" prune.job $prune_percent $metric $model_name $path $prunetask "random" $prunemethod
             done
         done
         # Note if it is balanced or unbalanced
