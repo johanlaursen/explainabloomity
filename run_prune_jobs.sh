fa3dc8fd799bb9fcc343596af679f241659f6423
Johan Christopher Løvschall Laursen <jocl@itu.dk>
2024-04-15 13:48:27 +0200

    fixed imbalanced pruning

--- run_prune_jobs.sh
+++ run_prune_jobs.sh
@@ -5,7 +5,7 @@
 # model_name="bigscience/bloom-7b1"
 # model_name="facebook/opt-6.7b"
 model_name="facebook/opt-13b"
-model_name="facebook/opt-13b"
+# model_name="facebook/opt-13b"
 
 model_basename="${model_name##*/}"
 path="/home/data_shares/mapillary/thesis_models/pruned_models/"
@@ -13,22 +13,23 @@ path="/home/data_shares/mapillary/thesis_models/pruned_models/"
 # Note if it is balanced or unbalanced need to change prune.job
 # prunetype="unbalanced_prune" # unbalanced_prune or pruned
 prune_methods=(
-    "balanced"
-    # "imbalanced"
+    # "balanced"
+    "imbalanced"
 )
 metrics=(
-    # "cosine"
+    "cosine"
     "euclidean"
 )
 prunetasks=(
-    #"paws_en"
+    "paws_en"
     "hellaswag"
     # "arc_easy"
+    "blimp_ellipsis_n_bar_1"
 )
 prune_percents=(
     "0.25"
-    # "0.5"
-    # "0.75"
+    "0.5"
+    "0.75"
 )
 
 for metric in "${metrics[@]}"
@@ -40,7 +41,7 @@ do
             for prunemethod in "${prune_methods[@]}"
             do
                 sbatch --job-name="${prunemethod}_${model_basename}_${metric}_${prune_percent}_${prompt}" prune.job $prune_percent $metric $model_name $path $prunetask $metric $prunemethod
-                # sbatch --job-name="${prunetype}_${model_basename}_${metric}_${prune_percent}_${prompt}" prune.job $prune_percent $metric $model_name $path $prunetask "random" $prunemethod
+                sbatch --job-name="${prunemethod}_${model_basename}_${metric}_${prune_percent}_${prompt}" prune.job $prune_percent $metric $model_name $path $prunetask "random" $prunemethod
             done
         done
         # Note if it is balanced or unbalanced
